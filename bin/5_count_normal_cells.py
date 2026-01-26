15fd62978d3f1959bfa6789158b171a267b67854
Anne Deslattes Mays <adeslat@scitechcon.org>
2026-01-26 00:32:35 +0000

    tell pandas new columns can hold any type (strings, numbers, empty strings, etc, line 270

--- bin/5_count_normal_cells.py
+++ bin/5_count_normal_cells.py
@@ -269,6 +269,9 @@ def process_all_datasets(input_csv, output_csv, logger):
     for col in new_columns:
         if col not in df.columns:
             df[col] = ''
+            
+    # Force all new columns to object dtype (allows any value type)
+    df[new_columns] = df[new_columns].astype(object)
     
     # Define column order
     column_order = [
